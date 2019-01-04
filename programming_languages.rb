328c7d7c3b19dfe2441c7c1ab86ab005cc26e3d6
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 23:02:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -23,7 +23,7 @@ end
 end
 
 narray.each do |lang|
-  if new_hash[lang][:style].has_value? == false
+  if new_hash[lang][:style].has_value?([:oo]) == true
     new_hash[lang][:style] = [:oo, :functional]
   else
   new_hash[lang][:style] = [:functional]
