19d1eae39b097dd8fe0c91e6c3e3154082613469
Heloise Taillet <ht2394@columbia.edu>
2019-01-04 22:59:19 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  array = []
+  new_hash= {}
+  narray = []
+
+languages[:oo].each do |language, type|
+  type.each do |typelabel, data|
+    array<<language
+    new_hash[language] = type
+end
+end
+array.each do |lang|
+  new_hash[lang][:style] = [:oo]
+end
+languages[:functional].each do |language, type|
+  type.each do |typelabel, data|
+    narray<<language
+    new_hash[language] = type
+end
+end
+
+narray.each do |lang|
+  if lang == :javascript
+    new_hash[lang][:style] = [:oo, :functional]
+  else
+  new_hash[lang][:style] = [:functional]
+end
+end
+ new_hash
 end
